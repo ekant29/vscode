2b972ce5ff70a67b260c244f26bfe3bb0f9fcc72
Ekant Rana <Ekant.Rana@smartenergywater.in>
2019-01-03 13:35:01 +0530

    First resource group created

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "azurerm" 
 {
-    azure_subscription_id = "${var.azure_subscription_id}"
-    azure_client_id = "${var.azure_subscription_id}"
-    azure_client_secret = "${var.azure_subscription_id}"
-    azure_tenant_id = "${var.azure_tenant_id}"
+    subscription_id = "${var.azure_subscription_id}"
+    client_id = "${var.azure_client_id}"
+    client_secret = "${var.azure_client_secret}"
+    tenant_id = "${var.azure_tenant_id}"
 }
